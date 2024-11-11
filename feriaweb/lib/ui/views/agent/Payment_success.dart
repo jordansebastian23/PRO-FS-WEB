b581dc01aae97d3eb3c4c6ed9779b84e59797a7c
Jordan Navarrete <jordan.navarrete@usm.cl>
2024-11-11 02:15:57 -0300

    Add DataPicker and changes UI

--- feriaweb/lib/ui/views/agent/Payment_success.dart
+++ feriaweb/lib/ui/views/agent/Payment_success.dart
@@ -1,8 +1,88 @@
+import 'package:feriaweb/constants/colors.dart';
 import 'package:feriaweb/datatables/payments_datasource.dart';
+import 'package:feriaweb/ui/buttons/custom_icontext_button.dart';
+import 'package:feriaweb/ui/inputs/custom_inputs.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class PaymentsSuccess extends StatelessWidget {
+
+class User {
+  final String nombre;
+  final String correo;
+
+  User({required this.nombre, required this.correo});
+}
+
+
+class PaymentsSuccess extends StatefulWidget {
   const PaymentsSuccess({super.key});
+
+  @override
+  State<PaymentsSuccess> createState() => _PaymentsSuccessState();
+}
+
+class _PaymentsSuccessState extends State<PaymentsSuccess> {
+  DateTime? _selectedDate;
+    User? selectedUser;
+    final TextEditingController _searchController = TextEditingController();
+    final ValueNotifier<List<User>> _filteredUsersNotifier = ValueNotifier<List<User>>([]);
+
+
+  List<User> allUsers = [
+    User(nombre: 'Usuario 1', correo: 'usuario1@example.com'),
+    User(nombre: 'Usuario 2', correo: 'usuario2@example.com'),
+    User(nombre: 'Usuario 3', correo: 'usuario3@example.com'),
+    User(nombre: 'Usuario 4', correo: 'usuario4@example.com'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredUsersNotifier.value = allUsers;
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _onSearchChanged() {
+    _filteredUsersNotifier.value = allUsers
+        .where((user) =>
+            user.nombre.toLowerCase().contains(_searchController.text.toLowerCase()) ||
+            user.correo.toLowerCase().contains(_searchController.text.toLowerCase()))
+        .toList();
+  }
+
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+      locale: const Locale('es', 'ES'),
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData.light().copyWith(
+            primaryColor: CustomColor.buttons, // Cambia el color primario
+            colorScheme: ColorScheme.light(primary: CustomColor.buttons), // Cambia el esquema de color
+            buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary), // Cambia el tema del botón
+          ),
+          child: child!,
+        );
+      },
+
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +97,35 @@ class PaymentsSuccess extends StatelessWidget {
               child: Container(
                 decoration: buildBoxDecoration(),
                 child: PaginatedDataTable(
+                  actions: [
+                    //filtrar por fecha field
+                    Row(
+                      children: [
+                        CustomIcontextButton(
+                            title: 'Filtrar por fecha',
+                            color: Colors.blueAccent,
+                            icon: Icons.calendar_month_outlined,
+                            onPressed: () => _selectDate(context)),
+                        //Reestablecer filtro
+                        CustomIcontextButton(
+                            title: 'Reestablecer filtro',
+                            color: Colors.redAccent,
+                            icon: Icons.replay_outlined,
+                            onPressed: () {
+                              setState(() {
+                                _selectedDate = null;
+                              });
+                            }),
+                        CustomIcontextButton(
+                            title: 'Crear Nuevo pago',
+                            color: CustomColor.buttons,
+                            icon: Icons.add,
+                            onPressed: () {
+                              _newPay(context);
+                            }),
+                      ],
+                    )
+                  ],
                   columnSpacing: 1,
                   columns: [
                     DataColumn(
@@ -40,7 +149,6 @@ class PaymentsSuccess extends StatelessWidget {
                     DataColumn(
                         label: Text('Metodo de pago',
                             style: TextStyle(fontWeight: FontWeight.bold))),
-                    
                     DataColumn(
                         label: Text('Detalles',
                             style: TextStyle(fontWeight: FontWeight.bold))),
@@ -51,9 +159,20 @@ class PaymentsSuccess extends StatelessWidget {
                     children: [
                       FittedBox(
                         fit: BoxFit.contain,
-                        child: Text('Pagos Realizados',
-                            style:
-                                TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Historial de pagos',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 24)),
+                            SizedBox(height: 10),
+                            //Fecha actual
+                            Text(
+                                'Fecha: ${_selectedDate != null ? _selectedDate!.toLocal().toString().split(' ')[0] : 'Seleccione una fecha'}',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 14)),
+                          ],
+                        ),
                       ),
                     ],
                   ),
@@ -67,11 +186,242 @@ class PaymentsSuccess extends StatelessWidget {
     );
   }
 
+  void _newPay(BuildContext context) {
+    String? amount;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Crear Nuevo Pago'),
+              content: SingleChildScrollView(
+                child: Container(
+                  width: 700,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Seleccionar Usuario: ',
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          SizedBox(width: 8),
+                          SizedBox(
+                            width: 200,
+                            child: TextField(
+                              enabled: false,
+                              decoration: CustomInputs.createUser(
+                                hint: 'Seleccione un usuario',
+                                label: selectedUser != null
+                                    ? selectedUser!.nombre
+                                    : 'Selecciona un usuario',
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.search),
+                            onPressed: () async {
+                              _showUserSelectionDialog(context, setState);
+                            },
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text('Ingresa monto [\$CLP]',
+                      style: GoogleFonts.poppins(fontWeight: FontWeight.w400, fontSize: 13),),
+                      SizedBox(height: 8),
+                      SizedBox(
+                        width: 140,
+                        height: 43,
+                        child: TextFormField(
+                          decoration: CustomInputs.createUser(
+                          colorBorder: Colors.black,
+                          hint: 'Ingresa el monto',
+                          label: 'Monto',),
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            amount = value;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              actions: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: CustomColor.buttons,
+                  ),
+                  child: Text('Cancelar',
+                      style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          fontSize: 16)),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: CustomColor.buttons,
+                  ),
+                  child: Text('Confirmar',
+                      style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          fontSize: 16)),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _showUserSelectionDialog(BuildContext context, StateSetter setState) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setStateDialog) {
+            return AlertDialog(
+              title: Text(
+                'Busqueda por Email',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(fontWeight: FontWeight.w400, fontSize: 13),
+              ),
+              content: SingleChildScrollView(
+                child: Container(
+                  width: 400,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text('Buscar: ',
+                              style: GoogleFonts.inter(
+                                  fontWeight: FontWeight.w500, fontSize: 20)),
+                          SizedBox(width: 5),
+                          Expanded(
+                            child: TextField(
+                              controller: _searchController,
+                              decoration: CustomInputs.searchBar(
+                                icon: Icons.search,
+                                colorBorder: Colors.black,
+                                hint: 'Ingresa el email o nombre del usuario',
+                                label: 'Buscar usuario',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      SizedBox(
+                        height: 200,
+                        child: ValueListenableBuilder<List<User>>(
+                          valueListenable: _filteredUsersNotifier,
+                          builder: (context, filteredUsers, _) {
+                            return ListView(
+                              children: filteredUsers
+                                  .map((user) => ListTile(
+                                        title: Row(
+                                          children: [
+                                            Checkbox(
+                                              value: selectedUser == user,
+                                              onChanged: (value) {
+                                                setStateDialog(() {
+                                                  selectedUser = user;
+                                                });
+                                                setState(() {
+                                                  selectedUser = user;
+                                                });
+                                                Navigator.of(context).pop(); // Cerrar diálogo
+                                              },
+                                            ),
+                                            SizedBox(width: 10),
+                                            CircleAvatar(),
+                                            SizedBox(width: 10),
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(user.nombre,
+                                                    style: GoogleFonts.inter(
+                                                        fontWeight: FontWeight.w600,
+                                                        fontSize: 14)),
+                                                Text(user.correo,
+                                                    style: GoogleFonts.inter(
+                                                        fontWeight: FontWeight.w400,
+                                                        fontSize: 14)),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                        onTap: () {
+                                          setStateDialog(() {
+                                            selectedUser = user;
+                                          });
+                                          setState(() {
+                                            selectedUser = user;
+                                          });
+                                          Navigator.of(context).pop(); // Cerrar diálogo
+                                        },
+                                      ))
+                                  .toList(),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              actions: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: CustomColor.buttons,
+                  ),
+                  child: Text('Cancelar',
+                      style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          fontSize: 16)),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: CustomColor.buttons,
+                  ),
+                  child: Text('Confirmar',
+                      style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          fontSize: 16)),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
   BoxDecoration buildBoxDecoration() => BoxDecoration(
-    color: Colors.white,
-    border: Border.all(color: Colors.black,
-        width: 1),
-    borderRadius: BorderRadius.circular(20),
-          
-          );
+        color: Colors.white,
+        border: Border.all(color: Colors.black, width: 1),
+        borderRadius: BorderRadius.circular(20),
+      );
 }
