d59284b20864c5e6d9d99a09e673a3953688eec2
Jordan Navarrete <jordan.navarrete@usm.cl>
2024-11-15 02:15:03 -0300

    Revert "build web"
    This reverts commit a42ed5cd8ea6ec94bbeaf14d84a731c9715166df.

--- feriaweb/build/web/flutter_service_worker.js
+++ feriaweb/build/web/flutter_service_worker.js
@@ -53,7 +53,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a7f0e5dcd4271e260cd47e67efefeb0d
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "f2064ea239233d6cf9f080824fae3c8a",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "b86dc88ac10960d0417c7ff40dd645d0",
+"flutter_bootstrap.js": "ba6d8545b05c327dc7a96d6b281ff533",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
